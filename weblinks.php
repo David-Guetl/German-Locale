1b24fe9d8f93b94d4f15b5ddd06fa5b82f247261
David Gütl <Webmaster@phpfusion-pro.com>
2011-07-28 12:53:15 +0300

    New Version for v7.03.00

--- weblinks.php
+++ weblinks.php
@@ -1,11 +1,10 @@
 <?php
 
-// Weblinks Allgemein
 $locale['400'] = "Weblinks";
 $locale['410'] = "[NEU]";
 $locale['411'] = "Hinzugef&uuml;gt am:";
 $locale['412'] = "Aufrufe:";
-$locale['430'] = "Keine Weblink Kategorien definiert.";
-$locale['431'] = "Es befinden sich keine Weblinks in dieser Kategorie.";
+$locale['430'] = "Es wurden leider noch keine Weblink Kategorien angelegt.";
+$locale['431'] = "Es wurden leider noch keine Weblinks in dieser Kategorie abgelegt.";
 
 ?>
