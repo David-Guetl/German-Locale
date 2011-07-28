9d96742649387777d14c081150e80a66b0836589
David Gütl <Webmaster@phpfusion-pro.com>
2011-07-28 06:47:25 +0300

    v7.03.00

--- articles.php
+++ articles.php
@@ -1,11 +1,11 @@
 <?php
 
 // Artikel Allgemein
-$locale['400'] = "Artikel";
-$locale['401'] = "Keine Artikel Kategorien definiert.";
+$locale['400'] = "Artikel &Uuml;bersicht";
+$locale['401'] = "Es wurden zum aktuellen Zeitpunkt leider noch keine Kategorien angelegt.";
 $locale['402'] = "NEU";
-$locale['403'] = "Es befinden sich keine Artikel in dieser Kategorie.";
-$locale['404'] = "Artikel Startseite";
-$locale['405'] = "Artikel Ordnung";
+$locale['403'] = "Es wurden leider noch keine Artikel in dieser Kategorie abgelegt.";
+$locale['404'] = "Artikel &Uuml;bersicht";
+$locale['405'] = "Artikel Navigation";
 
 ?>
