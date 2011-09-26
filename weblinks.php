89bfccd22efcf091f0239f19ed5b84a1790f75bd
David Gütl <Webmaster@phpfusion-pro.com>
2011-09-27 02:21:10 +0300

    Bug Fixes

--- weblinks.php
+++ weblinks.php
@@ -2,9 +2,9 @@
 
 $locale['400'] = "Weblinks";
 $locale['410'] = "[NEU]";
-$locale['411'] = "Hinzugef&uuml;gt am:";
+$locale['411'] = "Ver&ouml;ffentlichungsdatum:";
 $locale['412'] = "Aufrufe:";
-$locale['430'] = "Es wurden leider noch keine Weblink Kategorien angelegt.";
-$locale['431'] = "Es wurden leider noch keine Weblinks in dieser Kategorie abgelegt.";
+$locale['430'] = "Es wurden noch keine Weblink Kategorien angelegt.";
+$locale['431'] = "Es wurden noch keine Weblinks in dieser Kategorie abgelegt.";
 
 ?>
