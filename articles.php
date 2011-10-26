b9c47f347fb2d04b701adfaaa37521a3df6c2a0b
David Gütl <Webmaster@phpfusion-pro.com>
2011-10-26 22:55:41 +0300

    Bug Fixes

--- articles.php
+++ articles.php
@@ -2,10 +2,10 @@
 
 // Artikel Allgemein
 $locale['400'] = "Artikel";
-$locale['401'] = "Keine Artikel Kategorien definiert.";
-$locale['402'] = "NEU";
-$locale['403'] = "Es befinden sich keine Artikel in dieser Kategorie.";
-$locale['404'] = "Artikel Startseite";
-$locale['405'] = "Artikel Ordnung";
+$locale['401'] = "Es wurden noch keine Artikel Kategorien angelegt.";
+$locale['402'] = "[NEU]";
+$locale['403'] = "Es wurden noch keine Artikel in dieser Kategorie abgelegt.";
+$locale['404'] = "Artikel &Uuml;bersicht";
+$locale['405'] = "Artikel Navigation";
 
 ?>
