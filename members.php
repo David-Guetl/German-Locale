77f2d54f8e085528a9028731f564c8c502685484
David Gütl <Webmaster@phpfusion-pro.com>
2011-10-26 22:49:11 +0300

    Bug Fixes

--- members.php
+++ members.php
@@ -5,11 +5,9 @@ $locale['400'] = "Mitgliederliste";
 $locale['401'] = "Benutzername";
 $locale['402'] = "Benutzertyp";
 $locale['403'] = "Es existieren keine Benutzer beginnend mit ";
-$locale['404'] = "Alle anzeigen";
-
+$locale['404'] = "Alle";
 $locale['405'] = "Benutzergruppen";
 $locale['406'] = "In keiner Gruppe";
-
 $locale['407'] = "Zugriff auf alle Gruppen";
 
 ?>
