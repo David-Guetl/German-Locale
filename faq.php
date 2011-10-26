709466d15cbf0f9fd5a67c408949a5c39e04a56d
David Gütl <Webmaster@phpfusion-pro.com>
2011-10-26 22:53:23 +0300

    Bug Fixes

--- faq.php
+++ faq.php
@@ -4,7 +4,7 @@
 $locale['400'] = "FAQ (H&auml;ufig gestellte Fragen)";
 $locale['401'] = "FAQ";
 $locale['402'] = "Top";
-$locale['410'] = "Keine FAQ Kategorien definiert.";
-$locale['411'] = "Es befinden sich keine FAQs in dieser Kategorie.";
+$locale['410'] = "Es wurden noch keine FAQ Kategorien angelegt.";
+$locale['411'] = "Es wurden noch keine FAQs in dieser Kategorie abgelegt.";
 
 ?>
