7773d7436cd70c9fc739059a9d84c7808e726702
David Gütl <Webmaster@phpfusion-pro.com>
2011-10-26 22:51:40 +0300

    Bug Fixes

--- news_cats.php
+++ news_cats.php
@@ -5,9 +5,9 @@ $locale['400'] = "News Kategorien";
 $locale['401'] = "Kategorie:";
 $locale['402'] = "Enthaltene News:";
 $locale['403'] = "Unkategorisierte News";
-$locale['404'] = "Es befinden sich keine News in dieser Kategorie.";
+$locale['404'] = "Es wurden noch keine News in dieser Kategorie abgelegt.";
 $locale['405'] = "Mehr";
 $locale['406'] = "Alle Kategorien anzeigen";
-$locale['407'] = "Keine News Kategorien definiert.";
+$locale['407'] = "Es wurden noch keine News Kategorien angelegt.";
 
 ?>
