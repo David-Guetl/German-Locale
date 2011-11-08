edf5b4fd54933e9c2e2b2773dece59b0af22e7b2
David Gütl <Webmaster@phpfusion-pro.com>
2011-11-08 17:38:30 +0100

    Update comments.php

--- comments.php
+++ comments.php
@@ -10,6 +10,6 @@ $locale['c105'] = "Bitte logge dich ein, um ein Kommentar zu verfassen.";
 $locale['c106'] = "Kommentare verwalten";
 $locale['c107'] = " Smileys in diesen Kommentar deaktivieren.";
 $locale['c108'] = "Bearbeiten";
-$locale['c109'] = "L&ouml;schen";
+$locale['c109'] = "L&ouml;schen ";
 
 ?>
