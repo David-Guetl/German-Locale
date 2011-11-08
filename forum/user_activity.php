645cb475a9f9752c6a4d485d7d71f350f7c0014a
David Gütl <Webmaster@phpfusion-pro.com>
2011-11-08 21:36:37 +0100

    Bug Fixes

--- forum/user_activity.php
+++ forum/user_activity.php
@@ -1,13 +1,6 @@
 <?php
 
-// v7.03.xx Locale
-// Last Update: 25.06.2011
-
-if (!defined("IN_FUSION")) { 
-	header("Location: ../../../index.php");
-	exit;
-}
-
+// Foren Statistik
 $locale['forumActivity_stats']               = "Statistiken";
 $locale['forumActivity_threads']             = "Themen";
 $locale['forumActivity_threadspday']         = "Themen pro Tag";
